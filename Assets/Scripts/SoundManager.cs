df0260b80ea0bbd5b2fe4e412baa613cc55539ac
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2020-12-12 19:41:00 +0000

    Added volume controls
    Added music + sfx volume controls
    Added playerPrefs for volumes

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     List<AudioSource> sfx;
+    AudioSource music;
 
     private void Awake()
     {
@@ -13,10 +15,57 @@ public class SoundManager : MonoBehaviour
         {
             sfx.Add(transform.GetChild(i).GetComponent<AudioSource>());
         }
+        music = GetComponent<AudioSource>();
+
+        Debug.Log(PlayerPrefs.GetFloat("MusicValue"));
+        if (PlayerPrefs.HasKey("MusicValue"))
+        {
+            SetMusicVolume(PlayerPrefs.GetFloat("MusicValue"));
+        }
+        else { PlayerPrefs.SetFloat("MusicValue", 1f); }
+
+        if (PlayerPrefs.HasKey("SFXValue"))
+        {
+            SetSFXVolume(PlayerPrefs.GetFloat("SFXValue"));
+        }
+        else { PlayerPrefs.SetFloat("SFXValue", 1f); }
     }
 
     public void PlaySoundEffect(int effectNum)
     {
         sfx[effectNum].Play();
+    }
+
+    public void ChangeMusicVolume(GameObject musicSlider)
+    {
+        Slider musicVolume = musicSlider.GetComponent<Slider>();
+        music.volume = musicVolume.value;
+
+        PlayerPrefs.SetFloat("MusicValue", musicVolume.value);
+    }
+
+    public void SetMusicVolume(float newValue)
+    {
+        music.volume = newValue;
+        Debug.Log(newValue);
+    }
+
+    public void ChangeSFXVolume(GameObject sfxSlider)
+    {
+        Slider sfxVolume = sfxSlider.GetComponent<Slider>();
+        for (int i = 0; i < sfx.Count; i++)
+        {
+            sfx[i].volume = sfxVolume.value;
+        }
+
+        PlayerPrefs.SetFloat("SFXValue", sfxVolume.value);
+    }
+
+    public void SetSFXVolume(float newValue)
+    {
+        for (int i = 0; i < sfx.Count; i++)
+        {
+            sfx[i].volume = newValue;
+        }
     }
 }
